fb91ab0db2ba1d23444ade902950f82100bbd8eb
c29tZSBvbmU <86731433+c29tZSBvbmU@users.noreply.github.com>
2024-11-24 11:57:02 +0800

    Update DIY1.sh

--- script/DIY1.sh
+++ script/DIY1.sh
@@ -7,7 +7,7 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 sed -i 's/10.10.10.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # Define Default
-cat > package/yfdoorg/yfdoor/default-settings/files/zzz-default-settings <<-EOF
+#cat > package/yfdoorg/yfdoor/default-settings/files/zzz-default-settings <<-EOF
 #!/bin/sh    
     # change password
     sed -i '/root/d' /etc/shadow
